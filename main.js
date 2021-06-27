af75a207358cb7f354ea81aa6573c6ba24b581c6
Erick Castille <ecastille924@gmail.com>
2021-06-27 13:12:06 -0400

    Done.

--- main.js
+++ main.js
@@ -1,18 +1,64 @@
-// Defining text characters for the empty and full hearts for you to use later.
+// // Defining text characters for the empty and full hearts for you to use later.
+// const EMPTY_HEART = '♡'
+// const FULL_HEART = '♥'
+
+// // Your JavaScript code goes here!
+
+
+// const errorModal = document.getElementById("modal")
+// errorModal.className = "hidden"
+// const heartButtons = document.querySelectorAll(".like")
+
+
+// for (const button of heartButtons){
+//   button.addEventListener("click", handleClick)
+// }
+
+// function handleClick(e){
+//   if (e.target.innerText === EMPTY_HEART){
+//     mimicServerCall()
+//     .then(resp => {
+//       e.target.innerText = FULL_HEART
+//       e.target.className = "activated-heart"
+//       //debugger
+//     })
+//     .catch(error => {
+//       errorModal.className = ""
+//       errorModal.innerText = error
+//       setTimeout(()=> errorModal.className = "hidden",  5000)
+//     })
+//   }
+//     if (e.target.innerText === FULL_HEART) {
+//       e.target.innerText = EMPTY_HEART
+//       e.target.className = ""
+//   }
+// }
+
+// // When a user clicks on a full heart
+//   // Change the heart back to an empty heart
+
+//   // Remove the .activated-heart class
+
+
+// // Keep all your styling rules entirely in style.css. Do not manipulate any .style properties.
+// // Only manipulate the DOM once the server requests respond. Do not make the heart full until you're inside a successful .then block.
+
+// // > Note: The tests will only check for the first part of the specification (adding the hidden class). You should verify the rest of the behavior yourself, by checking the page in the browser.
+// //------------------------------------------------------------------------------
+// // Don't change the code below: this function mocks the server response
+// //------------------------------------------------------------------------------
+
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
-
-
-const errorModal = document.getElementById("modal")
-errorModal.className = "hidden"
+const errModal = document.getElementById("modal")
+errModal.className = "hidden"
 const heartButtons = document.querySelectorAll(".like")
-
 
 for (const button of heartButtons){
   button.addEventListener("click", handleClick)
 }
+
 
 function handleClick(e){
   if (e.target.innerText === EMPTY_HEART){
@@ -20,33 +66,18 @@ function handleClick(e){
     .then(resp => {
       e.target.innerText = FULL_HEART
       e.target.className = "activated-heart"
-      //debugger
     })
     .catch(error => {
-      errorModal.className = ""
-      errorModal.innerText = error
-      setTimeout(()=> errorModal.className = "hidden",  5000)
+      errModal.className = ""
+      errModal.innerText = error
+      setTimeout(()=> errModal.className = "hidden", 3000)
     })
   }
-    if (e.target.innerText === FULL_HEART) {
-      e.target.innerText = EMPTY_HEART
-      e.target.className = ""
+  if (e.target.innerText = FULL_HEART){
+    e.target.innerText = EMPTY_HEART
+    e.target.className = ""
   }
 }
-
-// When a user clicks on a full heart
-  // Change the heart back to an empty heart
-
-  // Remove the .activated-heart class
-
-
-// Keep all your styling rules entirely in style.css. Do not manipulate any .style properties.
-// Only manipulate the DOM once the server requests respond. Do not make the heart full until you're inside a successful .then block.
-
-// > Note: The tests will only check for the first part of the specification (adding the hidden class). You should verify the rest of the behavior yourself, by checking the page in the browser.
-//------------------------------------------------------------------------------
-// Don't change the code below: this function mocks the server response
-//------------------------------------------------------------------------------
 
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
